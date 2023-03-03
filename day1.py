e6cebff8ce09c37c916d9f99934f567e03122665
Khin Htet Myat Kyaw <105224019+KhinHtetMyatKyaw@users.noreply.github.com>
2023-03-03 10:57:04 +0630

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+marks=int(input("Please enter student marks:"))
+if  marks <= 0 or marks >= 100:
+        print ("Invalid Marks‼️")
+elif marks >=80 or marks==99:
+        print ("Honorable Marks🥳🎉(grade.🅰️ ✔️)")
+elif marks >= 50 or marks == 79:
+        print ("exam pass🎊(grade.🅱️ ✔️)")
+elif marks >=40 or marks ==50 :
+        print ("grade.C ✔️")
+elif marks <=39 :
+        print ("exam fail ❌")
+
+
+
+    
+
+        
+
